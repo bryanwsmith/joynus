7c8b002c5d1917bd545eca9e29a0c2c64b94967f
Riley Bracken <rileybracken@gmail.com>
2014-12-09 15:49:48 -0700

    Styles the route code to new version of rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "/:locale", locale: /en|es|kr/ do
-    get "/:id" => "high_voltage/pages#show", :as => :page, :format => false
+    get "/:id" => "high_voltage/pages#show", as: :page, format: false
   end
 
   get '/about' => 'high_voltage/pages#show', id: 'about'
