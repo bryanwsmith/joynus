7ee95c70cd418dbccd1a00b8fce4b00efdc520a6
Riley Bracken <rileybracken@gmail.com>
2014-12-16 13:54:03 -0700

    Adds the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  scope "/:locale", locale: /en|es|kr/ do
-    get "/:id" => "high_voltage/pages#show", as: :page, format: false
-  end
+  get "/:id" => "high_voltage/pages#show", as: :page, format: false
 end
