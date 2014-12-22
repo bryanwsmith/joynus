221b1dc95ea4064c1e60f961f960da9e775ddada
Riley Bracken <rileybracken@gmail.com>
2014-12-22 14:44:13 -0700

    Adds the user logged in navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def current_user?
+  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user?
+  helper_method :current_user
 
   private
 
