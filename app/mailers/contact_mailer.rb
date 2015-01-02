216eb01618915e5871d95d8c62da490a1707903c
Riley Bracken <rileybracken@gmail.com>
2015-01-02 10:16:03 -0700

    Finishes up emailing from the contact form.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,14 @@
 class ContactMailer < ActionMailer::Base
   def contact_email(first_name, last_name, email, subject, phone, message)
+    @to_email = Rails.env.development? ? 'riley@underbelly.is' : 'hello@underbelly.is'
     @first_name = first_name
     @last_name = last_name
     @email = email
     @subject = subject
+    @message = message
 
     mail subject: @subject,
-         to: 'riley@underbelly.is',
+         to: @to_email,
          from: @email
   end
 end
