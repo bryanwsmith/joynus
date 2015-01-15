d754903bdb41123894a26202acaa483849c9df4a
Riley Bracken <rileybracken@gmail.com>
2015-01-15 09:43:53 -0700

    Changes the translations again

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
   def contact_email(first_name, last_name, email, subject, phone, message)
-    @to_email = Rails.env.development? ? 'riley@underbelly.is' : 'hello@underbelly.is'
+    @to_email = Rails.env.development? ? 'hello@underbelly.is' : 'hello@underbelly.is'
     @first_name = first_name
     @last_name = last_name
     @email = email
