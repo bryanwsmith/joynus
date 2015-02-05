47cc2074c1ed344f8d4b0eecd1b1300c518b75d7
Riley Bracken <rileybracken@gmail.com>
2015-02-05 16:00:26 -0700

    Adds blog pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :authorize, only: [:edit, :update, :new, :create, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at").page(params[:page]).per(1)
   end
 
   def show
