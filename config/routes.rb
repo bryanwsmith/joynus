fee808b5690f34ce80a58869258285f45213e2ae
Ian Lee <carrey0728@gmail.com>
2018-08-24 13:17:37 +0000

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
 
+ 
+
   scope "(:locale)", locale: /en|es|kr/ do
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'
@@ -14,6 +16,7 @@ Rails.application.routes.draw do
     resources :employers, only: [:index]
     resources :contact, only: [:index]
     resources :services, only: [:index]
+    resources :staff, only: [:index]
     resources :unapproved_jobs
     resources :jobs do
       collection do
