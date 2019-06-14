1227449bc59d544e8708537854777840c0dde9b2
Chris Irish <irish@velocitylabs.io>
2019-06-13 18:43:59 -0700

    tidy

--- config/initializers/ckeditor_dragonfly.rb
+++ config/initializers/ckeditor_dragonfly.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Load Dragonfly if it isn't loaded already.
 require 'dragonfly'
 
--- db/migrate/20190614014155_create_ckeditor_assets.rb
+++ db/migrate/20190614014155_create_ckeditor_assets.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class CreateCkeditorAssets < ActiveRecord::Migration
-  def up
+  def self.up
     create_table :ckeditor_assets do |t|
       t.string  :data_uid, null: false
       t.string  :data_name, null: false
@@ -10,8 +8,8 @@ class CreateCkeditorAssets < ActiveRecord::Migration
       t.string  :type, limit: 30
 
       # Uncomment these to save image dimensions, if your need them.
-      # t.integer :data_width
-      # t.integer :data_height
+      t.integer :data_width
+      t.integer :data_height
 
       t.timestamps null: false
     end
@@ -19,7 +17,7 @@ class CreateCkeditorAssets < ActiveRecord::Migration
     add_index :ckeditor_assets, :type
   end
 
-  def down
+  def self.down
     drop_table :ckeditor_assets
   end
 end
